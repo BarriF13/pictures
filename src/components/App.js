ed3c1d59ee935b66dc9917b0a457e70ae345a043
Barri <info@bdesignstudio.co.uk>
2020-07-25 20:23:46 +0100

    Async function to a arrow function for bid reason

--- src/components/App.js
+++ src/components/App.js
@@ -5,24 +5,30 @@ import SearchBar from './SearchBar';
 
 
 class App extends Component {
+  // to show the response we need to have state so we init below
+  // we want to get images and they come in an array so we init in empty array
+  state = {
+    images: []
+  }
 
-
- async onSearchSubmit(term) {
+  onSearchSubmit = async (term)=> {
     //return term
     // console.log(term);
     // now adding axios to get the search request
     // we are getting -1 the address 2- object 
-  const  res = await axios.get('https://api.unsplash.com/search/photos', {
+    const res = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID HGZZbQmrfhvhBgq5VufA_HJ0rP1Fr6qyePvS8-yp_Kc'
       }
     })
-     
+
     // })
     // .then((res) => {
-      console.log(res.data.results);
+    // console.log(res.data.results);
     // })
+   
+    this.setState({ images: res.data.results});
 
   }
 
@@ -31,6 +37,7 @@ class App extends Component {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found : {this.state.images.length} images
       </div>
     );
   }
