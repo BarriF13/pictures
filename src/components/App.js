6abffc23fe9a27a2574a54674e614983c4110838
Barri <info@bdesignstudio.co.uk>
2020-07-25 19:59:30 +0100

    Get results with then

--- src/components/App.js
+++ src/components/App.js
@@ -12,18 +12,22 @@ class App extends Component {
     // console.log(term);
     // now adding axios to get the search request
     // we are getting -1 the address 2- object 
-    axios.get('https://api.unsplash.com/search/photos',{
-      params: {query : term},
-      headers:{
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
         Authorization: 'Client-ID HGZZbQmrfhvhBgq5VufA_HJ0rP1Fr6qyePvS8-yp_Kc'
       }
-    });
+    }).then((res) => {
+      console.log(res.data.results);
+    })
 
-   }
+  }
+
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react'
 
 export class SearchBar extends Component {
   //controlled element
-  state ={  term: ''  };
+  state = { term: '' };
   /* ** uncontrolled element :
   onInputChange(e){
     e.preventDefault();
     console.log(e.target.value);
   }
   */
- //2- changing function to arrow function do the binding  this.sate.term = this.state.bind(this);
-  onFormSubmit=(e)=>{
+  //2- changing function to arrow function do the binding  this.sate.term = this.state.bind(this);
+  onFormSubmit = (e) => {
     e.preventDefault();
     // console.log(this.state.term);
-   //becasue we are in class component we need to refer to it with this -- and we prop upping
+    //becasue we are in class component we need to refer to it with this -- and we prop upping
     this.props.onSubmit(this.state.term)
   }
   //3- third way of binding this.state.term -- 
@@ -22,23 +22,23 @@ export class SearchBar extends Component {
   //   console.log(this.state.term);
   // }
   render() {
-   
+
     return (
       <div className="ui segment ">
         {/* <form onSubmit={ (e)=>this.onFormSubmit(e)}  className="ui form"> this is for line third way */}
-        <form onSubmit={ this.onFormSubmit}  className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-          <label htmlFor="">Image Search</label>
-          <input 
-          type="text"
-          placeholder="Search"
-          //input already know the value but we put it down again
-          value={this.state.term}
-          // ** 1- onChange={this.onInputChange} 2- e will call 3- data will pass tp value on the line up because it keep changing the state by setState method
-          onChange={ e => this.setState({ term: e.target.value })}
-          />
-       
-          </div>  
+            <label htmlFor="">Image Search</label>
+            <input
+              type="text"
+              placeholder="Search"
+              //input already know the value but we put it down again
+              value={this.state.term}
+              // ** 1- onChange={this.onInputChange} 2- e will call 3- data will pass tp value on the line up because it keep changing the state by setState method
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+
+          </div>
         </form>
       </div>
     )
